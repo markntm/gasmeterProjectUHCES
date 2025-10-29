e26a33b6814cd767ed7c0e0b80fa7cb03fe7daf4
Mark Davis <markntm@hawaii.edu>
2025-10-29 09:32:12 -1000

    roboflow model???

--- GM_detection_cropping/detection_main.py
+++ GM_detection_cropping/detection_main.py
@@ -1,5 +1,23 @@
 from ultralytics import YOLO
+from secret import GMmodelV1
 import cv2
+from inference_sdk import InferenceHTTPClient
+
 
 model = YOLO("runs/detect/train/weights/best.pt")
 results = model("test_meter.jpg")
+
+
+# create an inference client
+CLIENT = InferenceHTTPClient(
+    api_url="https://detect.roboflow.com",
+    api_key="YOUR_API_KEY"
+)
+
+# run inference on a local image
+print(CLIENT.infer(
+    "your_image.jpg",
+    model_id="gauges_object_detection-9smis/2"
+))
+
+
