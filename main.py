c2596cb1eff04c2368fb620ba32e8db70d72e4c8
Mark Davis <markntm@hawaii.edu>
2025-10-31 09:19:19 -1000

    write out todo's

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ from GM_detection_cropping.detection_main import d_main
 if __name__ == "__main__":
     base_dir = Path(__file__).resolve().parent
     img_dir = base_dir / "GM_sample_images"
-    filename = img_dir / "GM4.png"  # <-- put your image path here
+    filename = img_dir / "GM3.png"  # <-- put your image path here
     image_path = str(filename)
 
     cropped_gauges = d_main(image_path)
